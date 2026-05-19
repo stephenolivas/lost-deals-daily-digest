201961a1ca0d6e46be67fff5d165563c1c2bd56c
stephenolivas <stephen@modern-amenities.com>
2026-05-19 16:07:08 -0700

    Update lost_deals_digest.py

--- lost_deals_digest.py
+++ lost_deals_digest.py
@@ -24,7 +24,7 @@ from zoneinfo import ZoneInfo
 CLOSE_API_KEY = os.environ["CLOSE_API_KEY"]
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
-SLACK_USER_ID = "U0A7QRN25S8"  # Michael Schultheiss
+SLACK_USER_ID = "U0AE7QUGP42"  # Michael Schultheiss
 LOST_REASON_FIELD = "cf_R4i05fLNOQP8yveAs4ofTMMYGAQnkLLklunP4lov2Bt"
 LOST_STATUS_LABEL = "💔 Lost"
 
@@ -69,8 +69,8 @@ def fetch_lost_status_changes(start_utc, end_utc):
     results = []
     skip = 0
     while True:
-        data = close_get("/activity/status_change/", params={
-            "date_created__gte": start_utc,
+        data = close_get("/activity/status_change/lead/", params={
+            "date_created__gt": start_utc,
             "date_created__lt": end_utc,
             "_limit": 100,
             "_skip": skip,
